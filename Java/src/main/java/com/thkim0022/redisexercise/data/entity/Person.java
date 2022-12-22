291a8018f79bf006144455398b267e1482f417b0
Taehyun Kim <k1m743hyun@icloud.com>
2022-12-22 17:22:00 +0900

    Create Java/src/test/java/com/k1m743hyun/redisexercise/repository/RedisRepositoryTest.java

--- Java/src/main/java/com/thkim0022/redisexercise/data/entity/Person.java
+++ Java/src/main/java/com/thkim0022/redisexercise/data/entity/Person.java
@@ -1,23 +1,48 @@
 package com.thkim0022.redisexercise.data.entity;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
-import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-@Getter
 @RedisHash(value = "people", timeToLive = 30)
-public class Person {
+public class Person implements Serializable {
 
     @Id
     private String id;
     private String name;
-    private Integer age;
+    private int age;
     private LocalDateTime createdAt;
 
-    public Person(String name, Integer age) {
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
-        this.createdAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
