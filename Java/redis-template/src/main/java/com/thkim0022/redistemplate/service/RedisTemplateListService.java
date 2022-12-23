84f623bfd0a4ac2e39c513d66a30af37136b8d1c
Taehyun Kim <k1m743hyun@icloud.com>
2022-12-23 16:56:06 +0900

    Update Java/redis-template/src/main/java/com/k1m743hyun/redistemplate/service/RedisTemplateListService.java

---
+++ Java/redis-template/src/main/java/com/thkim0022/redistemplate/service/RedisTemplateListService.java
@@ -0,0 +1,41 @@
+package com.thkim0022.redistemplate.service;
+
+import com.thkim0022.redistemplate.domain.PersonDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RedisTemplateListService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * List - Set
+     * @param requestDto
+     */
+    public void setPerson(PersonDto requestDto) {
+        log.info("{}.{}", getClass().getName(), Thread.currentThread().getStackTrace()[1].getMethodName());
+        //redisTemplate.opsForList().leftPush(requestDto.getKey(), requestDto);
+        redisTemplate.opsForList().rightPush(requestDto.getKey(), requestDto);
+    }
+
+    /**
+     * List - Get
+     * @param personId
+     * @return
+     */
+    public List<PersonDto> getPersonList(String personId) {
+        log.info("{}.{}", getClass().getName(), Thread.currentThread().getStackTrace()[1].getMethodName());
+        return redisTemplate.opsForList()
+            .range(PersonDto.keyOf(personId), 0, -1)
+            .stream()
+            .map(PersonDto::of)
+            .collect(Collectors.toList());
+    }
+}
