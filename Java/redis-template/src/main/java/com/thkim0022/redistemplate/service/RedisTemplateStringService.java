31582c3ce5169bae102000e18f471bb2fa85bb15
Taehyun Kim <k1m743hyun@icloud.com>
2022-12-23 16:11:32 +0900

    Create Java/redis-template/src/main/java/com/k1m743hyun/redistemplate/service/RedisTemplateStringService.java

--- Java/redis-template/src/main/java/com/thkim0022/redistemplate/service/RedisTemplateStringService.java
+++ Java/redis-template/src/main/java/com/thkim0022/redistemplate/service/RedisTemplateStringService.java
@@ -10,22 +10,32 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class RedisTemplateService {
+public class RedisTemplateStringService {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
-    // Set
-    public void savePerson(PersonDto requestDto) {
+    /**
+     * String - Set
+     * @param requestDto
+     */
+    public void setPerson(PersonDto requestDto) {
         redisTemplate.opsForValue().set(requestDto.getKey(), requestDto);
     }
 
-    // Get
+    /**
+     * String - Get
+     * @param personId
+     * @return
+     */
     public PersonDto getPerson(String personId) {
         return PersonDto.of(redisTemplate.opsForValue().get(PersonDto.keyOf(personId)));
     }
 
-    // Multi Set
-    public void savePersonList(List<PersonDto> requestDtos) {
+    /**
+     * String - Multi Set
+     * @param requestDtos
+     */
+    public void setPersonList(List<PersonDto> requestDtos) {
         Map<String, PersonDto> requestDtoMap = requestDtos.stream()
             .collect(Collectors.toMap(
                 PersonDto::getKey,
@@ -34,7 +44,11 @@ public class RedisTemplateService {
         redisTemplate.opsForValue().multiSet(requestDtoMap);
     }
 
-    // Multi Get
+    /**
+     * String - Multi Get
+     * @param personIds
+     * @return
+     */
     public List<PersonDto> getPersonList(List<String> personIds) {
         return redisTemplate.opsForValue()
             .multiGet(personIds.stream()
