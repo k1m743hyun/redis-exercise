401d7c567531617d9f884965efff7c1e2c119752
Taehyun Kim <k1m743hyun@icloud.com>
2022-12-23 16:55:48 +0900

    Update Java/redis-template/src/main/java/com/k1m743hyun/redistemplate/controller/RedisTemplateListController.java

---
+++ Java/redis-template/src/main/java/com/thkim0022/redistemplate/controller/RedisTemplateListController.java
@@ -0,0 +1,43 @@
+package com.thkim0022.redistemplate.controller;
+
+import com.thkim0022.redistemplate.domain.PersonDto;
+import com.thkim0022.redistemplate.service.RedisTemplateListService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/redis/list")
+public class RedisTemplateListController {
+
+    private final RedisTemplateListService service;
+
+    /**
+     * List - Set
+     * @param requestDto
+     */
+    @PostMapping
+    public void setPerson(@RequestBody PersonDto requestDto) {
+        log.info("{}.{}", getClass().getName(), Thread.currentThread().getStackTrace()[1].getMethodName());
+        service.setPerson(requestDto);
+    }
+
+    /**
+     * List - Get
+     * @param personId
+     * @return
+     */
+    @GetMapping("/{personId}")
+    public List<PersonDto> getPersonList(@PathVariable String personId) {
+        log.info("{}.{}", getClass().getName(), Thread.currentThread().getStackTrace()[1].getMethodName());
+        return service.getPersonList(personId);
+    }
+}
